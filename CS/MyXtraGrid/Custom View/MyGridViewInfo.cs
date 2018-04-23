fc35f0d3ddd2524886753adcfecd4b2a8d469952
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MyXtraGrid/Custom View/MyGridViewInfo.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Drawing;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+using DevExpress.XtraGrid.Views.Grid;
+
+namespace MyXtraGrid {
+	public class MyGridViewInfo : GridViewInfo {
+		public MyGridViewInfo(DevExpress.XtraGrid.Views.Grid.GridView gridView) : base(gridView) {}
+
+        public new MyGridView View { get { return base.View as MyGridView; } }
+
+        public override int CalcRowHeight(Graphics graphics, int rowHandle, int min, int level, bool useCache, GridColumnsInfo columns)
+        {
+            int height = base.CalcRowHeight(graphics, rowHandle, min, level, useCache, columns);
+            int paddingHeight = View.CellPadding.Vertical;
+            return height + paddingHeight;
+        }
+        public override int CalcColumnBestWidth(DevExpress.XtraGrid.Columns.GridColumn column)
+        {
+            int width = base.CalcColumnBestWidth(column);
+            int paddingWidth = View.CellPadding.Horizontal;
+            return width + paddingWidth;
+        }
+
+        protected override void CalcRowCellDrawInfoCore(GridDataRowInfo ri, DevExpress.XtraGrid.Drawing.GridColumnInfoArgs ci, GridCellInfo cell, DevExpress.XtraGrid.Drawing.GridColumnInfoArgs nextColumn, bool calcEditInfo)
+        {
+            base.CalcRowCellDrawInfoCore(ri, ci, cell, nextColumn, calcEditInfo);
+            Rectangle rect = cell.CellValueRect;
+            rect.X += View.CellPadding.Left;
+            rect.Width -= View.CellPadding.Horizontal;
+            rect.Y += View.CellPadding.Top;
+            rect.Height -= View.CellPadding.Vertical;
+            cell.CellValueRect = rect;
+        }
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to implement paddings for the GridView cells
+
+
+<p>This example demonstrates how to create a custom GridView with the CellPadding property to specify paddings for its cells</p>
+
+<br/>
+
+
